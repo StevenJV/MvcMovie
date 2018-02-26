304ccf10861c8a90c9f86ad8cbcedd731a36b3f8
StevenJV <StevenVore@tnwinc.com>
2018-02-26 11:21:33 -0500

    formatting cleanup, nothing to see here.

--- MvcMovie/Data/MvcMovieContext.cs
+++ MvcMovie/Data/MvcMovieContext.cs
@@ -6,13 +6,13 @@ using Microsoft.EntityFrameworkCore;
 
 namespace MvcMovie.Models
 {
-    public class MvcMovieContext : DbContext
+  public class MvcMovieContext : DbContext
+  {
+    public MvcMovieContext(DbContextOptions<MvcMovieContext> options)
+        : base(options)
     {
-        public MvcMovieContext (DbContextOptions<MvcMovieContext> options)
-            : base(options)
-        {
-        }
-
-        public DbSet<MvcMovie.Models.Movie> Movie { get; set; }
     }
+
+    public DbSet<MvcMovie.Models.Movie> Movie { get; set; }
+  }
 }
--- MvcMovie/Migrations/MvcMovieContextModelSnapshot.cs
+++ MvcMovie/Migrations/MvcMovieContextModelSnapshot.cs
@@ -10,34 +10,34 @@ using System;
 
 namespace MvcMovie.Migrations
 {
-    [DbContext(typeof(MvcMovieContext))]
-    partial class MvcMovieContextModelSnapshot : ModelSnapshot
+  [DbContext(typeof(MvcMovieContext))]
+  partial class MvcMovieContextModelSnapshot : ModelSnapshot
+  {
+    protected override void BuildModel(ModelBuilder modelBuilder)
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
-        {
 #pragma warning disable 612, 618
-            modelBuilder
-                .HasAnnotation("ProductVersion", "2.0.1-rtm-125")
-                .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
+      modelBuilder
+          .HasAnnotation("ProductVersion", "2.0.1-rtm-125")
+          .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
 
-            modelBuilder.Entity("MvcMovie.Models.Movie", b =>
-                {
-                    b.Property<int>("ID")
-                        .ValueGeneratedOnAdd();
+      modelBuilder.Entity("MvcMovie.Models.Movie", b =>
+          {
+            b.Property<int>("ID")
+                      .ValueGeneratedOnAdd();
 
-                    b.Property<string>("Genre");
+            b.Property<string>("Genre");
 
-                    b.Property<decimal>("Price");
+            b.Property<decimal>("Price");
 
-                    b.Property<DateTime>("ReleaseDate");
+            b.Property<DateTime>("ReleaseDate");
 
-                    b.Property<string>("Title");
+            b.Property<string>("Title");
 
-                    b.HasKey("ID");
+            b.HasKey("ID");
 
-                    b.ToTable("Movie");
-                });
+            b.ToTable("Movie");
+          });
 #pragma warning restore 612, 618
-        }
     }
+  }
 }
