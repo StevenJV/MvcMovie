4807b32b5c2ad9098ef1e25877e5f19165007170
StevenJV <StevenVore@tnwinc.com>
2018-03-09 14:14:05 -0500

    EF Migrations, craeted from NPM Console with `Add-Migration Rating` (also moved director-specific code to the Director migration, created earlier) run with `Update-Database`

--- MvcMovie/Migrations/20180226185052_director.cs
+++ MvcMovie/Migrations/20180226185052_director.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore.Migrations;
 using System;
 using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Metadata;
 
 namespace MvcMovie.Migrations
 {
@@ -8,12 +9,34 @@ namespace MvcMovie.Migrations
   {
     protected override void Up(MigrationBuilder migrationBuilder)
     {
+      migrationBuilder.AddColumn<int>(
+        name: "DirectorID",
+        table: "Movie",
+        nullable: false,
+        defaultValue: 0);
 
+      migrationBuilder.CreateTable(
+        name: "Director",
+        columns: table => new
+        {
+          Id = table.Column<int>(nullable: false)
+            .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+          Name = table.Column<string>(nullable: true)
+        },
+        constraints: table =>
+        {
+          table.PrimaryKey("PK_Director", x => x.Id);
+        });
     }
 
     protected override void Down(MigrationBuilder migrationBuilder)
     {
+      migrationBuilder.DropTable(
+        name: "Director");
 
+      migrationBuilder.DropColumn(
+        name: "DirectorID",
+        table: "Movie");
     }
   }
 }
